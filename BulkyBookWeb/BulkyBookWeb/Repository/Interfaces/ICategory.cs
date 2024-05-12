3a5f82b50e740d41124ac8a9b1c905f9580854be
Ismail <md.ismail@brainstation-23.com>
2024-05-12 23:27:44 +0600

    Add project files.

---
+++ BulkyBookWeb/BulkyBookWeb/Repository/Interfaces/ICategory.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Repository.Interfaces
+{
+    public interface ICategory<T>
+    {
+         Task<IEnumerable<T>> GetCategoriesAsync();
+         Task<T> AddCategory(T category);
+        
+         Task<T> EditCategory(int id);
+         Task<bool> UpdateCategory(T category);
+
+         Task<T> DeleteCategoryGet(int id);
+         Task<bool> DeleteCategoryPost(T category);
+
+         Task<T>Details(int id);
+    }
+}
---
+++ BulkyBookWeb/BulkyBookWeb/Repository/Services/CategoryService.cs
@@ -0,0 +1,91 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using BulkyBookWeb.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Repository.Services
+{
+    public class CategoryService<T> : ICategory<T> where T : class
+    {
+        public ApplicationDbContext context;
+        DbSet<T> dbSet;
+        public CategoryService(ApplicationDbContext context)
+        {
+                this.context = context;
+                dbSet =  context.Set<T>();
+        }
+
+        #region GetCategory
+        public async Task<IEnumerable<T>> GetCategoriesAsync()
+        {
+            var data = await dbSet.ToListAsync();
+
+            return data;
+        }
+        #endregion
+
+        #region Create
+        public async Task<T> AddCategory(T category)
+        {
+           await dbSet.AddAsync(category);
+           await context.SaveChangesAsync();
+            return category;
+        }
+        #endregion
+
+        #region Edit
+        public async Task<T> EditCategory(int id)
+        {
+           var data = await dbSet.FindAsync(id);
+            return data;
+        }
+        #endregion
+
+        #region Update
+        public async Task<bool> UpdateCategory(T category)
+        {
+            bool status = false;
+
+            if (category != null)
+            {
+                context.Update(category);
+                await context.SaveChangesAsync();
+                status = true;
+            }
+
+            return status;
+        }
+        #endregion
+
+        #region Delete
+        public async Task<T> DeleteCategoryGet(int id)
+        {
+            var data = await dbSet.FindAsync(id);
+
+            return data;
+        }
+
+        public async Task<bool> DeleteCategoryPost(T category)
+        {
+            var status = false;
+
+            if(category != null)
+            {
+                 dbSet.Remove(category);
+                 await context.SaveChangesAsync();
+                 status = true;
+            }
+
+            return status;
+        }
+        #endregion
+
+        #region Details
+        public async Task<T> Details(int id)
+        {
+            var data = await dbSet.FindAsync(id);
+            return data;
+        }
+        #endregion
+    }
+}
